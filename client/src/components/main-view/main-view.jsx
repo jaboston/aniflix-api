0e250bba213efc3c9e9610a7a76c77bfe368d77f
jaboston <jaboston89@gmail.com>
2019-07-29 11:58:35 +0200

    pulling data from public api

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import axios  from 'axios'
+import axios from 'axios'
 
 import { MovieCard } from '../movie-card/movie-card'
 import { MovieView } from '../movie-view/movie-view'
@@ -12,11 +12,22 @@ export class MainView extends React.Component {
     this.state = {
       movies: null,
       selectedMovie: null
-    };
+    }
   }
 
   componentDidMount () {
-    /* ... */
+    var BASE_URL = 'http://localhost:3000'
+
+    axios.get(BASE_URL + '/animes')
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        })
+      })
+      .catch(function (error) {
+        console.log(error)
+      })
   }
 
   onMovieClick (movie) {
@@ -29,12 +40,12 @@ export class MainView extends React.Component {
     const { movies, selectedMovie } = this.state;
 
     // Before the movies have been loaded
-    if (!movies) return <div className="main-view" >
+    if (!movies) return <div className='main-view' />
 
     return (
-     <div className="main-view">
+     <div className='main-view'>
       {selectedMovie
-         ? <MovieView movie={selectedMovie}/>
+         ? <MovieView movie={selectedMovie} />
          : movies.map(movie => (
            <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
          ))
